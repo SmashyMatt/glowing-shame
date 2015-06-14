178c83f339619043d1030cc237072dd1fbb0355e
Matt <matt@smashystudios.com>
2015-06-14 16:03:43 -0700

    More updates

---
+++ GlowingShame/Assets/Scripts/RollDice.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RollDice : MonoBehaviour {
+	public Rigidbody dice;
+	public float forceModifier = 3f;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void RollTriggered() {
+		Debug.Log ("flag");
+		Rigidbody diceInstance = (Rigidbody)Instantiate (dice, transform.position, transform.rotation);
+		diceInstance.velocity = transform.forward * forceModifier;
+		diceInstance.angularVelocity = transform.right * forceModifier;
+
+	}
+}
